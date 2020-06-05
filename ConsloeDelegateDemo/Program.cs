506247a0d63976f30a67411b38c45450d827f62f
ramakrishnamb <ramakrishnamb@gmail.com>
2020-06-05 18:09:42 +0530

    Multicast delegates

--- ConsloeDelegateDemo/Program.cs
+++ ConsloeDelegateDemo/Program.cs
@@ -6,8 +6,12 @@ namespace ConsloeDelegateDemo
     {
         //step1 : delcare a delegate
         private delegate void delPrintHello(string Msg);
+
+        private delegate void delMulticastdemo(string Msg);
+
         static void Main(string[] args)
         {
+            #region Delegate Basics
             // PrintMessage("Hello World!");
 
             //step2 : set delegate
@@ -25,9 +29,46 @@ namespace ConsloeDelegateDemo
             };
 
             //step3 : Inovke the delegate
-            printHello("Hello world called from Anonymous method!");
+            // printHello("Hello world called from Anonymous method!"); 
+            #endregion
+
+            #region Multicast Delegates
+            delMulticastdemo multicastdemo1 = ClassA.MethodA;
+            delMulticastdemo multicastdemo2 = ClassB.MethodB;
+
+            delMulticastdemo multicastdemoResult = multicastdemo1 + multicastdemo2;
+
+            //  multicastdemoResult("msg from multicast delegate");
+
+            delMulticastdemo delMulticastdemoLamda = (string Msg) => {
+                Console.WriteLine(Msg);
+            };
+            multicastdemoResult += delMulticastdemoLamda;
+
+            multicastdemoResult -= multicastdemo2;
+            multicastdemoResult("removed method B");
+            #endregion
+
+
             Console.ReadLine();
         }
+
+        class ClassA
+        {
+            internal static void MethodA(string message)
+            {
+                Console.WriteLine("Called ClassA.MethodA() with parameter: " + message);
+            }
+        }
+
+        class ClassB
+        {
+            internal static void MethodB(string message)
+            {
+                Console.WriteLine("Called ClassB.MethodB() with parameter: " + message);
+            }
+        }
+
 
         public static void PrintMessage(string Msg)
         {
